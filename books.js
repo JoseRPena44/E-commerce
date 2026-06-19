5a34559ad1533d1bfc8dadd721b8b34ac37eea90
JoseRPena44 <joserpena44@gmail.com>
2026-06-18 20:14:21 -0500

    Second Page Done

--- books.js
+++ books.js
@@ -1,7 +1,22 @@
-function renderBooks() {
-  const booksWrapper = document.querySelector('.books');
+let books;
 
- const books = getBooks();
+async  function renderBooks(filter) {
+ const booksWrapper = document.querySelector('.books');
+
+ booksWrapper.classList += " books__loading";
+ if (!books) {
+  books = await getBooks();
+ }
+ 
+ booksWrapper.classList.remove('books__loading');
+
+ if (filter === "LOW_TO_HIGH") {
+  books.sort((a, b) => ( a.salePrice || a.originalPrice) - ( b.salePrice || b.originalPrice));
+ } else if (filter === "HIGH_TO_LOW") {
+  books.sort((a, b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice));
+ } else if (filter === "RATING") {
+  books.sort((a, b) => b.rating - a.rating);
+ }
 
  const booksHtml = books.map((book) => {
    return `<div class="book">
@@ -12,26 +27,39 @@ function renderBooks() {
               ${book.title}
             </div>
             <div class="book__ratings">
-              <i class="fa-solid fa-star"></i>
-              <i class="fa-solid fa-star"></i>
-              <i class="fa-solid fa-star"></i>
-              <i class="fa-solid fa-star"></i>
-              <i class="fa-solid fa-star-half-alt"></i>
+              ${ratingsHTML(book.rating)}
             </div>
             <div class="book__price">
-              <span class=>$${book.originalPrice.toFixed(2)}</span> 
+              ${priceHTML(book.originalPrice, book.salePrice)}
             </div>
           </div>`;
-})
+      })
 .join(""); 
 
-console.log(booksHtml);
 
-booksWrapper.innerHTML = booksHtml;
+ booksWrapper.innerHTML = booksHtml;
+}
+
+function priceHTML (originalPrice, salePrice) {
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`;
+  }
+    return `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span> $${salePrice.toFixed(2)}`;
+}
+
+function ratingsHTML (rating) {
+  let ratings = "";
+  for (let i = 0; i < Math.floor(rating); i++) {
+    ratings += `<i class="fa-solid fa-star"></i>\n`;
+  }
+  if (!Number.isInteger(rating)) {
+    ratings += `<i class="fa-solid fa-star-half-alt"></i>\n`;
+  }
+  return ratings;
 }
 
 function filterBooks(event) {
-  console.log(event);
+  renderBooks(event.target.value);
 }
 
 setTimeout (() => {
@@ -40,7 +68,9 @@ setTimeout (() => {
 
 // FAKE DATA
 function getBooks() {
-  return [
+   return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
     {
       id: 1,
       title: "Crack the Coding Interview",
@@ -129,5 +159,7 @@ function getBooks() {
       salePrice: null,
       rating: 4.5,
     },
-  ];
+  ]);
+    }, 1000);
+  });
 }
